a872413ada0e735322b8f9934b2ee794421d6c64
Bruno Freire <brunofreire1717@gmail.com>
2023-01-08 00:06:49 -0500

    removeEVtheQuestions

--- index.js
+++ index.js
@@ -37,16 +37,16 @@ btnFt3.addEventListener('click', function(ev){
 
 import { enableOrDisableButton } from "./modules/enableOrDisableButton.js"
 
-document.getElementById('firstBtnQuestion').addEventListener('click', function(ev){
+document.getElementById('firstBtnQuestion').addEventListener('click', function(){
   enableOrDisableButton(1,'first',"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.")
 })
-document.getElementById('secondBtnQuestion').addEventListener('click', function(ev){
+document.getElementById('secondBtnQuestion').addEventListener('click', function(){
   enableOrDisableButton(2,"second","Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.")
 })
-document.getElementById('thirdBtnQuestion').addEventListener('click', function(ev){
+document.getElementById('thirdBtnQuestion').addEventListener('click', function(){
   enableOrDisableButton(3,"third","Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum.")
 })
-document.getElementById('fourthBtnQuestion').addEventListener('click', function(ev){
+document.getElementById('fourthBtnQuestion').addEventListener('click', function(){
   enableOrDisableButton(4,"fourth","Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.")
 })
 
